06ef9cc63f282d9bc4a81b6ffb90c6d5b13c93b2
Conor Watson <conor.watson94@gmail.com>
2025-12-02 22:55:05 +0000

    fix: logger check

--- logger.ts
+++ logger.ts
@@ -1,7 +1,7 @@
 export default {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   log: (message?: any, ...optionalParams: any[]) => {
-    if (process.env.ENABLE_LOGS) {
+    if (process.env.ENABLE_LOGS === 'true') {
       console.log(message, ...optionalParams);
     }
   },
