409706c5a164ec95d14201ae389f1afa794248c6
Conor Watson <conor.watson94@gmail.com>
2025-12-05 14:31:06 +0000

    feat: add chalk support to logger

--- logger.ts
+++ logger.ts
@@ -1,8 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import chalk from 'chalk';
+
 export default {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   log: (message?: any, ...optionalParams: any[]) => {
     if (process.env.ENABLE_LOGS === 'true') {
       console.log(message, ...optionalParams);
+    }
+  },
+  logGreen: (message?: any, ...optionalParams: any[]) => {
+    if (process.env.ENABLE_LOGS === 'true') {
+      console.log(chalk.green(message, ...optionalParams));
+    }
+  },
+  logRed: (message?: any, ...optionalParams: any[]) => {
+    if (process.env.ENABLE_LOGS === 'true') {
+      console.log(chalk.red(message, ...optionalParams));
     }
   },
 };
