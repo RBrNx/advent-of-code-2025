a661ff779f09fe8a21a725245605505ebafca1a1
Conor Watson <conor.watson94@gmail.com>
2025-12-18 11:49:35 +0000

    feat: day 07 part 2

--- 07/solve.ts
+++ 07/solve.ts
@@ -10,18 +10,22 @@ const partOne = (input: string[]) => {
       grid[currentRow + 1][startColumn] = '|';
       currentRow++;
     } else {
+      // Manually loop through each character in the current row
       for (let currentColumn = 0; currentColumn < grid[currentRow].length - 1; currentColumn++) {
         const currRowCell = grid[currentRow][currentColumn];
         const nextRowCell = grid[currentRow + 1][currentColumn];
 
+        // We can ignore empty space characters (.)
         if (currRowCell === '.') {
           continue;
         } else if (currRowCell === '|') {
+          // If we've found a beam and the cell directly underneath is a splitter, then we split the beam
           if (nextRowCell === '^') {
             grid[currentRow + 1][currentColumn - 1] = '|';
             grid[currentRow + 1][currentColumn + 1] = '|';
             numberOfSplits++;
           } else {
+            // Else we just keep moving the beam down
             grid[currentRow + 1][currentColumn] = '|';
           }
         }
@@ -34,7 +38,101 @@ const partOne = (input: string[]) => {
   return numberOfSplits;
 };
 
-const partTwo = (input: string[]) => {};
+const getNonEmptyIndexes = (array: string[]): number[] => {
+  const indexes = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== '.') {
+      indexes.push(i);
+    }
+  }
+
+  return indexes;
+};
+
+const followPath = (node: Node | null, visitedNodes: Map<string, number>): number => {
+  if (!node) return 0;
+
+  // Check if we've already visited this node
+  // if so we can simply return the timeline count we've already calculated in the past
+  if (visitedNodes.has(node.toString())) {
+    return visitedNodes.get(node.toString())!;
+  }
+
+  let intermediateTimelineCount = 0;
+
+  // If we've not got a left child then we can consider the beam has reached the end
+  if (!node.left) {
+    intermediateTimelineCount++;
+  }
+
+  // Same for right child
+  if (!node.right) {
+    intermediateTimelineCount++;
+  }
+
+  // Continue to follow the left/right paths
+  intermediateTimelineCount += followPath(node.left, visitedNodes);
+  intermediateTimelineCount += followPath(node.right, visitedNodes);
+
+  // We've now calculated the total timeline counts you can reach from this current node
+  // So lets store that for future in case we reach this node again
+  visitedNodes.set(node.toString(), intermediateTimelineCount);
+
+  return intermediateTimelineCount;
+};
+
+class Node {
+  position: [number, number] = [0, 0];
+  index: number = 0;
+  left: Node | null = null;
+  right: Node | null = null;
+
+  constructor(position: [number, number], index: number) {
+    this.position = position;
+    this.index = index;
+  }
+
+  toString() {
+    return `[${this.position[0]}, ${this.position[1]}]`;
+  }
+}
+
+const partTwo = (input: string[]) => {
+  const nodes = [];
+
+  // Convert input into an array of individual nodes with x, y coordinates
+  for (const [rowIndex, line] of input.entries()) {
+    if (rowIndex < 2) {
+      continue;
+    }
+
+    const indexesToProcess = getNonEmptyIndexes(line.split(''));
+
+    for (const columnIndex of indexesToProcess) {
+      const node = new Node([rowIndex, columnIndex], nodes.length + 1);
+      nodes.push(node);
+    }
+  }
+
+  // Loop through all nodes, attaching child nodes to parent ones based on their locations
+  for (const node of nodes) {
+    const [row, col] = node.position;
+
+    const leftNode = nodes.find(n => n.position[0] > row && n.position[1] === col - 1);
+    const rightNode = nodes.find(n => n.position[0] > row && n.position[1] === col + 1);
+
+    node.left = leftNode || null;
+    node.right = rightNode || null;
+  }
+
+  const visitedNodes = new Map<string, number>();
+
+  // Start following the possible paths from the root node
+  const numberOfTimelines = followPath(nodes[0], visitedNodes);
+
+  return numberOfTimelines;
+};
 
 const solve = (input: string[], part: number) => (part === 1 ? partOne(input) : partTwo(input));
 
