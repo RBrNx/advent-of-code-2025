bcdeb23011141de44deb918ece204e8a66c0201c
Conor Watson <conor.watson94@gmail.com>
2025-12-18 11:51:03 +0000

    fix: add expected answers for day 7

--- 07/solve.ts
+++ 07/solve.ts
@@ -110,7 +110,7 @@ const partTwo = (input: string[]) => {
     const indexesToProcess = getNonEmptyIndexes(line.split(''));
 
     for (const columnIndex of indexesToProcess) {
-      const node = new Node([rowIndex, columnIndex], nodes.length + 1);
+      const node: Node = new Node([rowIndex, columnIndex], nodes.length + 1);
       nodes.push(node);
     }
   }
@@ -136,6 +136,6 @@ const partTwo = (input: string[]) => {
 
 const solve = (input: string[], part: number) => (part === 1 ? partOne(input) : partTwo(input));
 
-const expected = (part: number) => (part === 1 ? 0 : 0);
+const expected = (part: number) => (part === 1 ? 1662 : 40941112789504);
 
 export { solve, expected };
