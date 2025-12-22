4c293db6290dc09c59d4c83ca93371d5b1dc3a9c
Conor Watson <conor.watson94@gmail.com>
2025-12-22 15:24:24 +0000

    feat: add expected answers for day 08

--- 08/solve.ts
+++ 08/solve.ts
@@ -136,6 +136,6 @@ const partTwo = (input: string[]) => {
 
 const solve = (input: string[], part: number) => (part === 1 ? partOne(input) : partTwo(input));
 
-const expected = (part: number) => (part === 1 ? 0 : 0);
+const expected = (part: number) => (part === 1 ? 57970 : 8520040659);
 
 export { solve, expected };
